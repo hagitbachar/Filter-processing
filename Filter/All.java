1c21d08925ccc209353dc7f18293eee3b3e5592a
hagit <42994615+hagitbachar@users.noreply.github.com>
2018-10-02 12:59:49 +0300

    Update All.java

--- Filter/All.java
+++ Filter/All.java
@@ -1,1 +1,20 @@
 
+package Filter;
+
+import java.io.*;
+
+/**
+ * Created by hagitba on 5/28/17.
+ */
+public class All implements FileFilter {
+
+    /**
+     * return every file
+     * @param pathname certain file that is checked
+     * @return true always
+     */
+    @Override
+    public boolean accept(java.io.File pathname) {
+        return true;
+    }
+}
